dda22ad596ca380c12847c4d44f9fd4ee1d7769a
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-12-06 14:33:59 +0100

    General settings and header initialization

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <HeaderApp />
+    <HeaderApp
+      :headerData="headerData"
+    />
     <MainApp />
     <FooterApp />
   </div>
@@ -18,10 +20,44 @@ export default {
     MainApp,
     FooterApp,
   },
+  data() {
+    return {
+      headerData: {
+        logo:
+          {
+            src: 'assets/img/MasterStudy-1.svg',
+            alt: 'Logo',
+          },
+        links: [
+          {
+            link: 'Courses',
+            href: '!#',
+          },
+          {
+            link: 'Course Format',
+            href: '!#',
+          },
+          {
+            link: 'Add Course',
+            href: '!#',
+          },
+          {
+            link: 'Pages',
+            href: '!#',
+          },
+          {
+            link: 'Demos',
+            href: '!#',
+          },
+        ],
+      },
+    };
+  },
 };
 </script>
 
 <style lang="scss">
+@import '@/assets/scss/variables';
 @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap');
 
@@ -37,6 +73,40 @@ html, body {
 
 h1, h2, h3, h4, h5, h6 {
   font-family: 'Roboto Slab', serif;
+  color: $text-color;
 }
 
+p, a {
+  color: $text-color;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+// CONTAINER GENERAL SETTING
+.container {
+  max-width: 850px;
+  height: 100%;
+  margin: 0 auto;
+}
+
+//FLEX PRE-SETTING
+.flex-row-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.flex-column-center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 </style>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,20 +1,37 @@
 <template>
-  <div>
-    <h1>Ciao io sono Roboto Slab gg</h1>
-    <p>Ciao io sono Roboto Semplice gg</p>
-    <!-- <font-awesome-icon icon="fa-brands fa-linkedin" />
-    <font-awesome-icon icon="fa-brands fa-instagram" />
-    <font-awesome-icon icon="fa-brands fa-facebook" />
-    <font-awesome-icon icon="fa-brands fa-twitter" /> -->
-  </div>
+  <header>
+    <div class="container flex-row-center">
+      <div class="logo">
+        <img :src="headerData.logo.src" :alt="headerData.logo.alt">
+      </div>
+      <div class="links">
+        <nav>
+          <ul>
+            <li v-for="obj in headerData.links" :key="obj.link">
+              <a href="#">{{ obj.link }}</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: 'HeaderApp',
+  props: {
+    headerData: Object,
+  },
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+header {
+  max-width: 100%;
+  height: 100px;
+}
+.container {
+  background-color: lightblue;
+}
 </style>

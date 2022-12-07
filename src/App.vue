b1a7f1b8c6dd947d5ad3f5a18cf38dbebeeb84a0
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-12-07 02:37:51 +0100

    Add Sign section + improvements

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <HeaderApp
       :headerData="headerData"
     />
-    <MainApp />
+    <MainApp
+      :mainData="mainData"
+    />
     <FooterApp />
   </div>
 </template>
@@ -73,6 +75,36 @@ export default {
             href: '#!',
           },
         ],
+      },
+      mainData: {
+        heroSection:
+          {
+            hero_image: 'img/slide-1.jpg',
+            title_first: 'Start your Human',
+            title_color: 'Revolution',
+            title_second: 'Today',
+            text: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of sping which I enjoy with my whole heart.',
+          },
+        counterDate:
+          {
+            text:
+            {
+              title_first: 'Limited Sale: ',
+              title_second: 'All courses with 55% off',
+            },
+            year: 2022,
+            mounth: 11,
+            day: 25,
+            hour: 0,
+            minute: 0,
+            second: 0,
+          },
+        signSection:
+          {
+            presentation_text: 'Hew there, my name is John Doe I\'m the founder of the Brain Academy.',
+            firm_src: 'img/sign.jpg',
+            text: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary',
+          },
       },
     };
   },
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,13 +1,114 @@
 <template>
-  <h1>Hello Main</h1>
+  <main>
+    <!-----------------
+        HERO SECTION
+    ------------------->
+    <section class="hero-section"
+      :style="{backgroundImage: `url(${mainData.heroSection.hero_image})`}">
+      <div class="text-container">
+        <h1>
+          {{ mainData.heroSection.title_first }}
+          <span> {{ mainData.heroSection.title_color }} </span>
+          {{ mainData.heroSection.title_second }}
+        </h1>
+        <p>{{ mainData.heroSection.text }}</p>
+      </div>
+      <MailCounter
+        :counterDate="mainData.counterDate"
+      />
+    </section>
+
+    <!-----------------
+        FIRM SECTION
+    ------------------->
+    <section class="sign-section flex-row-center">
+      <div class="container flex-row-center">
+        <div class="presentation flex-column-center">
+          <h3>
+            {{ mainData.signSection.presentation_text }}
+          </h3>
+          <img :src="mainData.signSection.firm_src" alt="">
+        </div>
+        <div class="presentation-text">
+          <p>{{ mainData.signSection.text }}</p>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
+import MailCounter from '@/components/MailCounter.vue';
+
 export default {
   name: 'MainApp',
+  components: {
+    MailCounter,
+  },
+  props: {
+    mainData: Object,
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/assets/scss/variables';
+@import '@/assets/scss/reset';
+.hero-section {
+  height: 565px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
 
+  .text-container {
+    max-width: 420px; //variable
+    position: absolute;
+    left: 45%;
+    bottom: 50%;
+
+    h1 {
+      margin-bottom: 1rem;
+      line-height: 1;
+
+      span {
+        color: $buttercup;
+      }
+    }
+
+    p {
+      color: $dove-gray;
+      font-size: 14px;
+      line-height: 2;
+    }
+  }
+}
+.sign-section {
+  height: 405px;
+
+  .container {
+    margin-top: 1.5rem;
+    gap: 4rem;
+
+    .presentation {
+      flex: 1 1 40%;
+
+      img {
+        margin: 2rem 5rem 0 0;
+        align-self: flex-end;
+        max-width: 100px;
+      }
+    }
+
+    .presentation-text {
+      flex: 1 1 60%;
+
+      p {
+        font-weight: 300;
+        font-size: 13px;
+        line-height: 2;
+      }
+    }
+  }
+}
 </style>
